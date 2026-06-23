404a779676d6c4073f7998f596e1362b82ad6cd7
NocturnalTomato <96393958+NocturnalTomato@users.noreply.github.com>
2026-06-23 12:13:39 +0200

    Use dedicated web assets directory for Capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'nl.groengeel.hokrat',
   appName: 'HO Krat',
-  webDir: '.',
+  webDir: 'www',
   bundledWebRuntime: false,
   server: {
     androidScheme: 'https'
